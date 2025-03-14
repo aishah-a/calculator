8a88164f1f9b82e3c665ff8e736da8b7ce138234
aishah-a <aishahahmeed@gmail.com>
2025-03-14 19:06:23 +0000

    Add button functionality and working display

--- script.js
+++ script.js
@@ -31,6 +31,99 @@ function operate(opr1, opr2, operator) {
     }
 }
 
-let operand1;
-let operand2;
+let operand1 = [];
+let operand2 = [];
 let operator;
+let answer;
+
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+
+
+numbers.forEach(number => { 
+    number.addEventListener("click", () => {
+        if (operator === undefined) {
+            operand1.push(number.textContent);
+            console.log(operand1);
+        } else {
+                operand2.push(number.textContent);
+                console.log(operand2);
+            }
+    })
+});
+
+numbers.forEach(number => { 
+    number.addEventListener("click", () => {
+        if (display.textContent == "0") {
+            display.textContent = number.textContent
+        } else if (operator !== undefined) {
+            display.textContent = "";
+            display.textContent += number.textContent
+        }
+    })
+})
+
+
+operators.forEach(op => {
+    op.addEventListener("click", () => {
+        if (answer !== undefined) {
+            console.log("there's an answer");
+            operand1 = [];
+            operand1.push(answer);
+            operand2 = [];
+        }
+    })
+})
+
+
+operators.forEach(op => { 
+    op.addEventListener("click", () => {
+        if (operator === undefined || 
+            operator !== undefined && answer !== undefined) {
+            switch (op.textContent) {
+                case "+":
+                    operator = "add";
+                break;
+                case "-":
+                    operator = "subtract";
+                break;
+                case "*":
+                    operator = "multiply";
+                break;
+                case "/":
+                    operator = "divide";
+                break;
+            }
+            console.log(operator);
+            return operator;
+        }
+    }) 
+});
+
+equals.addEventListener("click", () => {
+    if (answer === undefined) {
+        operand1 = +operand1.join("");
+        operand2 = +operand2.join("");
+    
+        answer = operate(operand1, operand2, operator);
+        console.log(operate(operand1, operand2, operator));
+        display.textContent = answer
+        return answer;
+    }
+})
+
+clear.addEventListener("click", () => {
+    operand1 = [];
+    operand2 = [];
+    operator = undefined;
+    display.textContent = 0
+})
+
+
+
+
+
+
