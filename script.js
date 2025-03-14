f6f2b9bf02faa662dd8ca4ecb002d37bd3e2a03a
aishah-a <aishahahmeed@gmail.com>
2025-03-14 21:23:45 +0000

    Write script to chain operations with equals sign

--- script.js
+++ script.js
@@ -32,9 +32,11 @@ function operate(opr1, opr2, operator) {
 }
 
 let operand1 = [];
+let num1;
 let operand2 = [];
+let num2;
 let operator;
-let answer;
+let answer
 
 const display = document.querySelector("#display");
 const numbers = document.querySelectorAll(".num");
@@ -42,33 +44,125 @@ const operators = document.querySelectorAll(".operator");
 const clear = document.querySelector("#clear");
 const equals = document.querySelector("#equals");
 
-
 numbers.forEach(number => { 
     number.addEventListener("click", () => {
+        if (answer !== undefined) {
+            operand2 = [];
+        }
         if (operator === undefined) {
             operand1.push(number.textContent);
             console.log(operand1);
-        } else {
-                operand2.push(number.textContent);
-                console.log(operand2);
-            }
-    })
-});
-
-numbers.forEach(number => { 
-    number.addEventListener("click", () => {
-        if (display.textContent == "0") {
-            display.textContent = number.textContent
-        } else if (operator !== undefined) {
-            display.textContent = "";
-            display.textContent += number.textContent
+        }   else {
+            operand2.push(number.textContent);
+            console.log(operand2);
         }
     })
 })
 
 
+operators.forEach(oper => {
+    oper.addEventListener("click", () => {
+        if (operator !== undefined) {
+            console.log("answer alr");
+            calculate();
+            operand1 = [];
+            operand1.push(answer);
+        }
+        switch (oper.textContent) {
+            case "+":
+                operator = "add";
+            break;
+            case "-":
+                operator = "subtract";
+            break;
+            case "*":
+                operator = "multiply";
+            break;
+            case "/":
+                operator = "divide";
+            break;
+        }
+        console.log(operator);
+        })
+})
+
+// press 4 - op1 [4]
+// press ADD - oper add
+// press 3 - op2 [3]
+// press ADD - LOG answer = op1 (oper) op2 [7]
+//    op1 = answer [7]
+//    oper = ADD
+//    op2 = []
+// press 5 - op2 [5]
+// press EQUALS - answer = 12
+
+
+
+function normalize() {
+    if (typeof operand1 !== "number") {
+        num1 = operand1.join("");
+    } if (typeof operand2 !== "number") {
+        num2 = operand2.join("");
+    }
+}
+
+function calculate() {
+    normalize();
+    answer = operate(+num1, +num2, operator);
+    console.log(answer);
+}
+
+
+equals.addEventListener("click", () => {
+    calculate();
+})
+
+// numbers.forEach(number => { 
+//     number.addEventListener("click", () => {
+//         // if (operator === undefined) {
+//         if (operand1.length === 0) {
+//             operand1.push(number.textContent);
+//             console.log(operand1);
+//         } else {
+//                 operand2.push(number.textContent);
+//                 console.log(operand2);
+//             }
+//     })
+// });
+
+// numbers.forEach(number => { 
+//     number.addEventListener("click", () => {
+//         if (display.textContent == "0") {
+//             display.textContent = number.textContent
+//         } else if (operator !== undefined) {
+//             display.textContent = "";
+//             display.textContent += number.textContent
+//         } else {
+//             display.textContent += number.textContent
+//         }
+//     })
+// })
+
+// press 6 - operand1 [6]
+// press ADD - operator add
+// press 3 - operand2 [3]
+// press ADD - operand1 = operate(operand1, operand2, operator), operator = ADD
+// press 1 - operand 2 [1]
+
+/*
+
 operators.forEach(op => {
     op.addEventListener("click", () => {
+        // if (operand1.length !== 0 && operand2.length !== 0) {
+        //     operand1 = +operand1.join("");
+        //     operand2 = +operand2.join("");
+            
+        //     answer = operate(operand1, operand2, operator);
+        //     console.log(answer);
+        //     display.textContent = answer
+        //     return answer;
+        // } 
+        
         if (answer !== undefined) {
             console.log("there's an answer");
             operand1 = [];
@@ -79,48 +173,19 @@ operators.forEach(op => {
 })
 
 
-operators.forEach(op => { 
-    op.addEventListener("click", () => {
-        if (operator === undefined || 
-            operator !== undefined && answer !== undefined) {
-            switch (op.textContent) {
-                case "+":
-                    operator = "add";
-                break;
-                case "-":
-                    operator = "subtract";
-                break;
-                case "*":
-                    operator = "multiply";
-                break;
-                case "/":
-                    operator = "divide";
-                break;
-            }
-            console.log(operator);
-            return operator;
-        }
-    }) 
-});
-
 equals.addEventListener("click", () => {
-    if (answer === undefined) {
-        operand1 = +operand1.join("");
-        operand2 = +operand2.join("");
-    
-        answer = operate(operand1, operand2, operator);
-        console.log(operate(operand1, operand2, operator));
-        display.textContent = answer
-        return answer;
-    } else {
-        operand1 = +answer;
-        operand2 = +operand2.join("");
-        answer = operate(operand1, operand2, operator);
-        display.textContent = answer
-        return answer;
+    if (answer !== undefined) {
+    operand1 = +operand1.join("");
+    operand2 = +operand2.join("");
 
+    answer = operate(operand1, operand2, operator);
+    console.log(answer);
+    display.textContent = answer;
     }
 })
+
+
+
 
 clear.addEventListener("click", () => {
     operand1 = [];
@@ -129,8 +194,4 @@ clear.addEventListener("click", () => {
     display.textContent = 0
 })
 
-
-
-
-
-
+*/
