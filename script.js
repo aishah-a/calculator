3b56bbc1dea41c07890f0f2734fd46de5642aed8
aishah-a <aishahahmeed@gmail.com>
2025-03-16 17:34:59 +0100

    Sort code by functions and declarations

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".operator");
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+const decimal = document.querySelector("#decimal");
+
+let operand1 = [];
+let num1;
+let operand2 = [];
+let num2;
+let operator;
+let answer;
+
 function add(a, b) {
     return a + b;
 }
@@ -35,22 +49,6 @@ function operate(opr1, opr2, operator) {
     }
 }
 
-let operand1 = [];
-let num1;
-let operand2 = [];
-let num2;
-let operator;
-let answer;
-
-const display = document.querySelector("#display");
-const numbers = document.querySelectorAll(".num");
-const operators = document.querySelectorAll(".operator");
-const clear = document.querySelector("#clear");
-const equals = document.querySelector("#equals");
-const decimal = document.querySelector("#decimal");
-
-decimal.addEventListener("click", addDecimal)
-
 function addDecimal() {
     if (operand1.length === 0) {
         operand1.push("0");
@@ -74,6 +72,38 @@ function addDecimal() {
     }
 }
 
+function normalize() {
+    if (typeof operand1 !== "number") {
+        num1 = operand1.join("");
+    } if (typeof operand2 !== "number") {
+        num2 = operand2.join("");
+    }
+}
+
+function calculate() {
+    normalize();
+    answer = operate(+num1, +num2, operator);
+    console.log(answer);
+    display.textContent = answer;
+}
+
+function updateDisplay(number) {
+    {
+        if (operand1.includes(".") === true && operator === undefined) {
+            display.textContent += number.textContent;
+        } else if (display.textContent == 0 && operator === undefined) {
+            display.textContent = "";
+            display.textContent += number.textContent;
+        } else if (operand2.length === 1 && operator !== undefined && !operand2.includes(".")) {
+            display.textContent = "";
+            display.textContent = number.textContent;
+        } else if (operand2.length !== 0 && operator !== undefined) {
+            display.textContent += number.textContent;
+        } else if (operator === undefined) {
+            display.textContent += number.textContent;
+        }
+    }
+}
 
 numbers.forEach(number => { 
     number.addEventListener("click", () => {
@@ -121,45 +151,13 @@ operators.forEach(oper => {
         })
 })
 
-function normalize() {
-    if (typeof operand1 !== "number") {
-        num1 = operand1.join("");
-    } if (typeof operand2 !== "number") {
-        num2 = operand2.join("");
-    }
-}
-
-function calculate() {
-    normalize();
-    answer = operate(+num1, +num2, operator);
-    console.log(answer);
-    display.textContent = answer;
-}
+decimal.addEventListener("click", addDecimal)
 
 equals.addEventListener("click", () => {
     calculate();
     operand1 = answer.toString().split("");
     operand2 = [];
 })
-
-
-function updateDisplay(number) {
-    {
-        if (operand1.includes(".") === true && operator === undefined) {
-            display.textContent += number.textContent;
-        } else if (display.textContent == 0 && operator === undefined) {
-            display.textContent = "";
-            display.textContent += number.textContent;
-        } else if (operand2.length === 1 && operator !== undefined && !operand2.includes(".")) {
-            display.textContent = "";
-            display.textContent = number.textContent;
-        } else if (operand2.length !== 0 && operator !== undefined) {
-            display.textContent += number.textContent;
-        } else if (operator === undefined) {
-            display.textContent += number.textContent;
-        }
-    }
-}
 
 clear.addEventListener("click", () => {
     operand1 = [];
